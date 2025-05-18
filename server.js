4b073cd8a51eaa348491736b574d5258f91abb94
SHAFICAHAMED <shafic1111ahamed@gmail.com>
2025-05-18 09:03:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ service: 'gmail', auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_
 
 // Cron Job: every day at 12 AM
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
 
 const today = new Date(); const users = await User.find(); users.forEach(user => { const birthDate = new Date(user.birthday); if (birthDate.getDate() === today.getDate() && birthDate.getMonth() === today.getMonth()) { const mailOptions = { from: process.env.EMAIL_USER, to: user.email, subject: '🎉 Wishing You the Happiest Birthday!!', text: `Dear ${user.name},\n 
 
